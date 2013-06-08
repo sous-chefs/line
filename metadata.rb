3dc6c8404e0637350322129274e5cd251426ca68
Sean OMeara <someara@opscode.com>
2013-06-08 07:58:09 -0400

    bumping to 0.4.3

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "someara@opscode.com"
 license          "Apache2"
 description      "Provides line editing resources for use by recipes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.2"
+version          "0.4.3"
 
