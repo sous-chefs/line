7dd13bdf7943dc8ca1ded67e49feb169434ad52c
Jeff Blaine <jblaine@kickflop.net>
2014-08-25 20:13:52 -0400

    Flesh out chefspec tests
    This commit removes 2 of the 3 canned files that were put in
    place via cookbook_file (dangerfile and dangerfile2) in favor
    of using the 'content' attribute on a 'file' resource instead
    inside recipes/tester.rb. The canned file serial.conf was kept
    as-is since it is large.
    
    We perform more extensive ChefSpec testing now via the new-ish
    render_file('/some/path').with_content(/Regex-here/) helper.
    When there is a .without_content(...) helper we can add tests
    for that.

--- spec/tester_spec.rb
+++ spec/tester_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
+# Historical Note: For those coming across this cookbook, these
+# filenames were of the original author's doing (Seam O'Meara).
+# There is no special meaning behind them, so don't be confused.
+# He's just weird like the rest of us.
+
 describe 'line::tester' do
   let(:chef_run) { ChefSpec::Runner.new.converge 'line::tester' }
 
-  it 'creates dangerfile' do
-    expect(chef_run).to create_cookbook_file('/tmp/dangerfile').with_owner('root').with_mode('00644')
+  it 'creates dangerfile with HELLO as first word on a line' do
+    expect(chef_run).to render_file('/tmp/dangerfile').with_content(/^HELLO.*/)
   end
-  
+
+  it 'creates dangerfile with line "# UNCOMMENT ME YOU FOOL"' do
+    expect(chef_run).to render_file('/tmp/dangerfile').with_content(/^# UNCOMMENT ME YOU FOOL$/)
+  end
+
+  it 'creates dangerfile2 with at least 1 line starting "ssh"' do
+    expect(chef_run).to render_file('/tmp/dangerfile2').with_content(/^ssh.*/)
+  end
+
+  it 'creates serial.conf' do
+    expect(chef_run).to render_file('/tmp/serial.conf')
+  end
 end
 
