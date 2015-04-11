288d0526cbf89819a8cf54085f33c4482734ee76
Dang H. Nguyen <haidangwa@gmail.com>
2015-04-10 19:01:11 -0700

    added supports data

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ description      'Provides line editing resources for use by recipes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.7.0'
 
+supports 'ubuntu'
+supports 'centos', 'redhat'
