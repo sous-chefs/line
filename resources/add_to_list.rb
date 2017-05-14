38ad31ae77021fb01bec24a97d4ec673dda337a9
Dan Webb <dan.webb@damacus.io>
2017-05-14 17:05:48 +0100

    Straight swap to custome_resources

---
+++ resources/add_to_list.rb
@@ -0,0 +1,87 @@
+property :name, String, name_property: true
+property :path, String
+property :pattern, String
+property :delim, Array
+property :entry, String
+property :ends_with, String
+
+resource_name :add_to_list
+
+action :edit do
+  ends_with = new_resource.ends_with ? Regexp.escape(new_resource.ends_with) : ''
+  regex = /#{new_resource.pattern}.*#{ends_with}/
+
+  if ::File.exist?(new_resource.path)
+    begin
+      f = ::File.open(new_resource.path, 'r+')
+
+      file_owner = f.lstat.uid
+      file_group = f.lstat.gid
+      file_mode = f.lstat.mode
+
+      temp_file = Tempfile.new('foo')
+
+      modified = false
+
+      regexdelim = []
+      new_resource.delim.each do |delim|
+        regexdelim << escape_regex(delim)
+      end
+
+      f.each_line do |line|
+        if line =~ regex
+          found = true
+          if new_resource.ends_with
+            list_end = line.rindex(new_resource.ends_with)
+            seperator = line =~ /#{new_resource.pattern}.*\S.*#{ends_with}/ ? new_resource.delim[0] : ''
+            case new_resource.delim.count
+            when 1
+              next if line =~ /(#{regexdelim[0]}|#{new_resource.pattern})\s*#{new_resource.entry}\s*(#regexdelim[0]|#{ends_with})/
+              line = line.chomp.insert(list_end, "#{seperator}#{new_resource.entry}")
+              modified = true
+            when 2
+              next if line =~ /#{regexdelim[1]}#{new_resource.entry}\s*#{regexdelim[1]}/
+              line = line.chomp.insert(list_end, "#{seperator}#{new_resource.delim[1]}#{new_resource.entry}#{new_resource.delim[1]}")
+              modified = true
+            when 3
+              next if line =~ /#{regexdelim[1]}#{new_resource.entry}\s*#{regexdelim[2]}/
+              line = line.chomp.insert(list_end, "#{seperator}#{new_resource.delim[1]}#{new_resource.entry}#{new_resource.delim[2]}")
+              modified = true
+            end
+          else
+            seperator = line =~ /#{new_resource.pattern}.*\S.*$/ ? new_resource.delim[0] : ''
+            case new_resource.delim.count
+            when 1
+              next if line =~ /((#{regexdelim[0]})*|#{new_resource.pattern})\s*#{new_resource.entry}(#{regexdelim[0]}|\n)/
+              line = line.chomp + "#{seperator}#{new_resource.entry}"
+              modified = true
+            when 2
+              next if line =~ /#{regexdelim[1]}#{new_resource.entry}#{regexdelim[1]}/
+              line = line.chomp + "#{seperator}#{new_resource.delim[1]}#{new_resource.entry}#{new_resource.delim[1]}"
+              modified = true
+            when 3
+              next if line =~ /#{regexdelim[1]}#{new_resource.entry}#{regexdelim[2]}/
+              line = line.chomp + "#{seperator}#{new_resource.delim[1]}#{new_resource.entry}#{new_resource.delim[2]}"
+              modified = true
+            end
+          end
+        end
+        temp_file.puts line
+      end
+
+      f.close
+
+      if modified
+        temp_file.rewind
+        FileUtils.copy_file(temp_file.path, new_resource.path)
+        FileUtils.chown(file_owner, file_group, new_resource.path)
+        FileUtils.chmod(file_mode, new_resource.path)
+        new_resource.updated_by_last_action(true)
+      end
+
+    ensure
+      temp_file.close
+      temp_file.unlink
+    end
+  end # if ::File.exists?
+end
---
+++ resources/replace_or_add.rb
@@ -0,0 +1,70 @@
+property :name, name_property:true
+property :path, String
+property :pattern, String
+property :line, String
+property :replace_only, [true, false]
+
+resource_name :replace_or_add
+
+action :edit do
+  regex = /#{new_resource.pattern}/
+
+  if ::File.exist?(new_resource.path)
+    begin
+      f = ::File.open(new_resource.path, 'r+')
+
+      file_owner = f.lstat.uid
+      file_group = f.lstat.gid
+      file_mode = f.lstat.mode
+
+      temp_file = Tempfile.new('foo')
+
+      modified = false
+      found = false
+
+      f.each_line do |line|
+        if line =~ regex || line.chomp == new_resource.line
+          found = true
+          unless line.chomp == new_resource.line
+            line = new_resource.line
+            modified = true
+          end
+        end
+        temp_file.puts line
+      end
+
+      unless found || new_resource.replace_only # "add"!
+        temp_file.puts new_resource.line
+        modified = true
+      end
+
+      f.close
+
+      if modified
+        temp_file.rewind
+        FileUtils.copy_file(temp_file.path, new_resource.path)
+        FileUtils.chown(file_owner, file_group, new_resource.path)
+        FileUtils.chmod(file_mode, new_resource.path)
+        new_resource.updated_by_last_action(true)
+      end
+
+    ensure
+      temp_file.close
+      temp_file.unlink
+    end
+  else
+
+    begin
+      nf = ::File.open(new_resource.path, 'w')
+      unless new_resource.replace_only
+        nf.puts new_resource.line
+        new_resource.updated_by_last_action(true)
+      end
+    rescue ENOENT
+      Chef::Log.info('ERROR: Containing directory does not exist for #{nf.class}')
+    ensure
+      nf.close
+    end
+
+  end # if ::File.exists?
+end
