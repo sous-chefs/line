27514822748c8e075b982186401ad132cfb7b76c
markgibbons <mark.gibbons@nordstrom.com>
2017-07-05 06:05:28 -0700

    Delete minitest files
    Minitest is no longer used for integration tests. Delete the leftover files.

--- files/default/tests/minitest/default_test.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path('../support/helpers', __FILE__)
-
-describe 'line::default' do
-  include Helpers::Line
-
-  # Example spec tests can be found at http://git.io/Fahwsw
-  it 'runs no tests by default' do
-  end
-end
