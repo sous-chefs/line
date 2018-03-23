febfbbc2d2f778c0186641d9613f41d6edd0154e
Lamont Granquist <lamont@scriptkiddie.org>
2018-03-23 11:46:54 -0700

    Use Regexp.escape() and add a comment
    the helper class here is already baked directly into ruby, and should
    really get removed overall.
    
    commented out a subtle thing i did which i think is the right thing to
    do because it makes the resource more declarative and the user doesn't
    need to thing about files-with-terminating-CRs, but i have a small
    doubt about adding the CR before matching, so just documented that.
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- resources/append_if_no_line.rb
+++ resources/append_if_no_line.rb
@@ -4,18 +4,18 @@ property :line, String
 resource_name :append_if_no_line
 
 action :edit do
-  string = escape_string new_resource.line
+  string = Regexp.escape(new_resource.line)
   regex = /^#{string}$/
 
   current = ::File.readlines(new_resource.path)
+  # we match the regexp after doing this append for files without terminating CRs.  should
+  # we instead match against the unchanged content?  we're basically saying "don't worry
+  # about terminating CRs or not, we gotcha covered" which feels like the 99% use case.  but
+  # is there a 1% use case here which considers this a bug?
   current[-1] = current[-1].chomp + "\n"
 
   file new_resource.path do
     content((current + [new_resource.line + "\n"]).join)
     not_if { ::File.exist?(new_resource.path) && !current.grep(regex).empty? }
   end
-end
-
-action_class.class_eval do
-  include Line::Helper
 end
