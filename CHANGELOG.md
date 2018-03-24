751aaff0ab19e272f25677cdae2c0e874e2e1c43
Dan Webb <dan.webb@damacus.io>
2018-03-24 15:02:07 +0000

    Update CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,14 @@
 # line Cookbook CHANGELOG
 
+
 ## 1.0.7 (2018-03-23)
 
 - Rework `delete_lines` to use file provider subresource.
 - Support matching with regexps in addition to strings with `delete_lines`.
 - Rework `append_if_no_line` to use file provider subresource.
 - Fix edge conditions around files-with-no-trailing-CR being fed to `append_if_no_line`.
+- Remove library helpers.
+- Moves the `escape_string` method into the resources using it
 
 ## 1.0.6 (2018-03-23)
 
--- resources/replace_or_add.rb
+++ resources/replace_or_add.rb
@@ -8,54 +8,69 @@ resource_name :replace_or_add
 action :edit do
   regex = new_resource.pattern.is_a?(String) ? /#{new_resource.pattern}/ : new_resource.pattern
 
-  raise "File #{new_resource.path} not found" unless ::File.exist?(new_resource.path)
+  if ::File.exist?(new_resource.path)
+    begin
+      f = ::File.open(new_resource.path, 'r+')
 
-  begin
-    f = ::File.open(new_resource.path, 'r+')
+      file_owner = f.lstat.uid
+      file_group = f.lstat.gid
+      file_mode = f.lstat.mode
 
-    file_owner = f.lstat.uid
-    file_group = f.lstat.gid
-    file_mode = f.lstat.mode
+      temp_file = Tempfile.new('foo')
 
-    temp_file = Tempfile.new('foo')
+      modified = false
+      found = false
 
-    modified = false
-    found = false
+      f.each_line do |line|
+        if line =~ regex || line.chomp == new_resource.line
+          found = true
+          unless line.chomp == new_resource.line
+            line = new_resource.line
+            modified = true
+          end
+        end
 
-    f.each_line do |line|
-      if line =~ regex || line.chomp == new_resource.line
-        found = true
-        unless line.chomp == new_resource.line
-          line = new_resource.line
-          modified = true
+        temp_file.puts line
+      end
+
+      unless found || new_resource.replace_only # "add"!
+        temp_file.puts new_resource.line
+        modified = true
+      end
+
+      f.close
+
+      if modified
+        converge_by "Updating file #{new_resource.path}" do
+          temp_file.rewind
+          FileUtils.copy_file(temp_file.path, new_resource.path)
+          FileUtils.chown(file_owner, file_group, new_resource.path)
+          FileUtils.chmod(file_mode, new_resource.path)
         end
       end
-
-      temp_file.puts line
+    ensure
+      temp_file.close
+      temp_file.unlink
     end
-
-    unless found || new_resource.replace_only # "add"!
-      temp_file.puts new_resource.line
-      modified = true
-    end
-
-    f.close
-
-    if modified
+  else
+    unless new_resource.replace_only
       converge_by "Updating file #{new_resource.path}" do
-        temp_file.rewind
-        FileUtils.copy_file(temp_file.path, new_resource.path)
-        FileUtils.chown(file_owner, file_group, new_resource.path)
-        FileUtils.chmod(file_mode, new_resource.path)
+        begin
+          nf = ::File.open(new_resource.path, 'w')
+          nf.puts new_resource.line
+        rescue ENOENT
+          Chef::Log.info('ERROR: Containing directory does not exist for #{nf.class}')
+        ensure
+          nf.close
+        end
       end
     end
-  ensure
-    temp_file.close
-    temp_file.unlink
   end
 end
 
 action_class.class_eval do
   require 'fileutils'
   require 'tempfile'
+
+  include Line::Helper
 end
