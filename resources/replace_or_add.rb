31b1da56e426c1eaba4945b4e85cfe0ab0f7020b
Dan Webb <dan.webb@damacus.io>
2018-03-24 12:50:49 +0000

    Add raise if file not found to every resource

--- resources/replace_or_add.rb
+++ resources/replace_or_add.rb
@@ -8,61 +8,61 @@ resource_name :replace_or_add
 action :edit do
   regex = new_resource.pattern.is_a?(String) ? /#{new_resource.pattern}/ : new_resource.pattern
 
-  if ::File.exist?(new_resource.path)
-    begin
-      f = ::File.open(new_resource.path, 'r+')
+  raise "File #{new_resource.path} not found" unless ::File.exist?(new_resource.path)
 
-      file_owner = f.lstat.uid
-      file_group = f.lstat.gid
-      file_mode = f.lstat.mode
+  begin
+    f = ::File.open(new_resource.path, 'r+')
 
-      temp_file = Tempfile.new('foo')
+    file_owner = f.lstat.uid
+    file_group = f.lstat.gid
+    file_mode = f.lstat.mode
 
-      modified = false
-      found = false
+    temp_file = Tempfile.new('foo')
 
-      f.each_line do |line|
-        if line =~ regex || line.chomp == new_resource.line
-          found = true
-          unless line.chomp == new_resource.line
-            line = new_resource.line
-            modified = true
-          end
-        end
+    modified = false
+    found = false
 
-        temp_file.puts line
-      end
-
-      unless found || new_resource.replace_only # "add"!
-        temp_file.puts new_resource.line
-        modified = true
-      end
-
-      f.close
-
-      if modified
-        converge_by "Updating file #{new_resource.path}" do
-          temp_file.rewind
-          FileUtils.copy_file(temp_file.path, new_resource.path)
-          FileUtils.chown(file_owner, file_group, new_resource.path)
-          FileUtils.chmod(file_mode, new_resource.path)
+    f.each_line do |line|
+      if line =~ regex || line.chomp == new_resource.line
+        found = true
+        unless line.chomp == new_resource.line
+          line = new_resource.line
+          modified = true
         end
       end
-    ensure
-      temp_file.close
-      temp_file.unlink
+
+      temp_file.puts line
     end
-  else
-    unless new_resource.replace_only
+
+    unless found || new_resource.replace_only # "add"!
+      temp_file.puts new_resource.line
+      modified = true
+    end
+
+    f.close
+
+    if modified
       converge_by "Updating file #{new_resource.path}" do
-        begin
-          nf = ::File.open(new_resource.path, 'w')
-          nf.puts new_resource.line
-        rescue ENOENT
-          Chef::Log.info('ERROR: Containing directory does not exist for #{nf.class}')
-        ensure
-          nf.close
-        end
+        temp_file.rewind
+        FileUtils.copy_file(temp_file.path, new_resource.path)
+        FileUtils.chown(file_owner, file_group, new_resource.path)
+        FileUtils.chmod(file_mode, new_resource.path)
+      end
+    end
+  ensure
+    temp_file.close
+    temp_file.unlink
+  end
+else
+  unless new_resource.replace_only
+    converge_by "Updating file #{new_resource.path}" do
+      begin
+        nf = ::File.open(new_resource.path, 'w')
+        nf.puts new_resource.line
+      rescue ENOENT
+        Chef::Log.info('ERROR: Containing directory does not exist for #{nf.class}')
+      ensure
+        nf.close
       end
     end
   end
