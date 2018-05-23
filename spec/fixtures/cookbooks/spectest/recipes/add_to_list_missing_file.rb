f2422accdd1d87295fff8fd9ff7058bda73c9951
markgibbons <mark.gibbons@nordstrom.com>
2018-05-22 20:18:36 -0500

    Fix the missing file test.
    Move the chefspec test recipes to spec/fixtures
    Merge the missing file tests with the other tests for each provider

--- spec/fixtures/cookbooks/spectest/recipes/add_to_list_missing_file.rb
+++ spec/fixtures/cookbooks/spectest/recipes/add_to_list_missing_file.rb
@@ -1,8 +1,3 @@
-file 'prep for test /tmp/nofilehere' do
-  path '/tmp/nofilehere'
-  action :delete
-end
-
 add_to_list 'missing_file fail' do
   path '/tmp/nofilehere'
   pattern Regexp.escape('empty_delimited_list=(')
