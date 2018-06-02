ebb5b8cdc450fe3c5c1688c099a217ac447b745f
markgibbons <mark.gibbons@nordstrom.com>
2018-06-02 11:31:02 -0700

    Add stanza processing
    Limited stanza syntax
    Need refactoring and a DRY review

--- spec/unit/library/filter/stanza_spec.rb
+++ spec/unit/library/filter/stanza_spec.rb
@@ -29,20 +29,35 @@ describe 'stanza method' do
       ]
     )
   end
- 
+
   it 'should add stanza to an existing array' do
     out_array = @current.dup
     ip = out_array.size
     out_array[ip] = '[newstanza]'
-    out_array[ip+1] = '  key1 = value1'
-    out_array[ip+2] = '  key2 = value2'
+    out_array[ip + 1] = '  key1 = value1'
+    out_array[ip + 2] = '  key2 = value2'
     expect(@filt.stanza(@current.dup, ['newstanza', { key1: 'value1', key2: 'value2' }])).to eq(out_array)
   end
- 
-  it 'should replace values in an existing array' do
+
+  it 'should replace values in a stanza' do
     out_array = @current.dup
-    out_array[3] = '  key1 = replace1'
+    out_array[2] = '  key1 = replace1'
     expect(@filt.stanza(@current.dup, ['head1', { key1: 'replace1' }])).to eq(out_array)
+  end
+
+  it 'should replace multiple values in a stanza' do
+    out_array = @current.dup
+    out_array[6] = '  key2 = replace2'
+    out_array[7] = '  key3 = replace3'
+    expect(@filt.stanza(@current.dup, ['head2', { key2: 'replace2', key3: 'replace3' }])).to eq(out_array)
+  end
+
+  it 'should replace and add values in a stanza' do
+    out_array = @current.dup
+    out_array[9] = '  newkey = insertvalue'
+    out_array[10] = '  key3.1 = replace1'
+    out_array[11] = '[head4]'
+    expect(@filt.stanza(@current.dup, ['head3', { 'key3.1' => 'replace1', newkey: 'insertvalue' }])).to eq(out_array)
   end
 
 end
