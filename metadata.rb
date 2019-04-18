472e7d1e5b1864268af603136bf2563238a6b99d
markgibbons <mark.gibbons@nordstrom.com>
2019-04-17 21:38:54 -0700

    Add a test to verify that the linked files exist

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license          'Apache-2.0'
 description      'Provides line editing resources for use by recipes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.3.1'
-source_url       'https://github.com/sous-chefs/line-cookbook'
-issues_url       'https://github.com/sous-chefs/line-cookbook/issues'
+source_url       'https://github.com/sous-chefs/line'
+issues_url       'https://github.com/sous-chefs/line/issues'
 chef_version     '>= 12.13.37'
 
 %w(debian ubuntu centos redhat scientific oracle amazon windows).each do |os|
