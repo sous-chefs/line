8d746bc589af3a8f9371ced19cea3c735e7cfee4
Tim Smith <tsmith84@gmail.com>
2019-10-13 20:23:53 -0700

    Minor Cookstyle fixes
    Autocorrected with cookstyle 5.8
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Provides line editing resources for use by recipes'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.4.1'
 source_url       'https://github.com/sous-chefs/line'
 issues_url       'https://github.com/sous-chefs/line/issues'
